e3f76df46b5222e111c875b2372f004af6d1d75f
James Hagood <cardfaux@tutanota.com>
2020-01-19 12:35:00 -0600

    first commit

--- app.js
+++ app.js
@@ -70,3 +70,36 @@ const printBio = person.printBio;
 //-------------------------------------------------------------------------------------
 //                              ARROW FUNCTIONS AND THIS
 //-------------------------------------------------------------------------------------
+
+// REFER TO LINE 58
+
+const annoyer = {
+	phrases: [
+		'literally',
+		'cray cray',
+		'I cant even',
+		'Totes',
+		'YOLO',
+		"Can't Stop, Won't Stop"
+	],
+	pickPhrase() {
+		const { phrases } = this;
+		const idx = Math.floor(Math.random() * phrases.length);
+		return phrases[idx];
+	},
+	// start() {
+	// 	console.log(this.pickPhrase())
+	// 	setInterval(function() {
+	// 		console.log(this.pickPhrase())                    //HERE THIS IS SET TO THE WINDOW WHEN NOT RAN AS AN ARROW FUNCTION
+	// 	}, 3000)
+	// },
+	start() {
+		this.timerId = setInterval(() => {
+			console.log(this.pickPhrase()); //HERE THIS IS SET TO THE OBJECT BECAUSE IT REFRES TO THE THIS FROM START
+		}, 3000);
+	},
+	stop() {
+		clearInterval(this.timerId);
+		console.log('PHEW THANK HEAVENS THAT IS OVER!');
+	}
+};
